b14e72d4cf026ed232f7587ee5fe4ec30b78e996
oskarSveinsson <oskarsveinsson99@gmail.com>
2025-05-07 14:42:50 +0000

    user profile and login. VERY basic

--- castle_apartments/settings.py
+++ castle_apartments/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/users/profile/' #change later maybe
