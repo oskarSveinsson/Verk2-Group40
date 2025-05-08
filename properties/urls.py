7d4b191f1fcc37808683032ff1b9d7954b75ea2f
oskarSveinsson <oskarsveinsson99@gmail.com>
2025-05-08 16:22:17 +0000

    front page v0.2

--- properties/urls.py
+++ properties/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='properties-index'),
-    path('<int:id>', views.get_properties_by_id, name='properties-by-id'),
+    path('', views.get_properties, name='properties-index'),
+    path('<int:id>', views.get_property_detail, name='property-detail'),
 ]
